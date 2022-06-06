66d0b7cf698393f43f355eec26dd4d347add145a
munenebrian <njerubrianmunene@gmail.com>
2022-06-06 20:33:34 +0300

    commit tree 8b2931fa2fc6f5b61465eaf144a19aa7df70a83c

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'string';
+let welcomeMessage = 'Welcome to Flatbook, string!';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, STRING!';
+const shortGreeting = 'Welcome, s!';
